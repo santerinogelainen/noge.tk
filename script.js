9733b12a7e7f25d19a26814bf9dac0cd40aa8a3c
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2016-01-29 14:22:25 +0200

    menu items css transitions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$(".link").mouseenter(function(){
-		$(this).css("background-color", "#0d0d0d");
+		$(this).css("background-color", "black");
 	});
 	$(".link").mouseleave(function(){
 		$(this).css("background-color", "inherit");
