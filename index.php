a18f58a43dbcce15d2ea1857602889b4366187ab
Santeri Nogelainen <santeri.nogelainen@gmail.com>
2016-02-15 12:08:12 +0200

    added google font

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" type="text/css" href='styles.css'/>
 	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Abril+Fatface&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
 	<!-- links end -->
 	
 	<script src="jquery/jquery-1.12.0.min.js"></script>
